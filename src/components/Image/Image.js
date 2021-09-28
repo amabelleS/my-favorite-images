2c7c14c470359fdd20e492ca1ec4219d01d72a7e
amabelle <amabelletr@gmail.com>
2021-09-29 00:28:51 +0300

    Importing abbreviateNumber from utils

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,59 +1,23 @@
 import React, { useContext } from 'react';
 import Context from '../../context/favorites/context';
+import { abbreviateNumber } from '../../utils';
 
 import * as S from './style';
-
-function abbreviateNumber(value) {
-  let newValue = value;
-  if (value >= 1000) {
-    let suffixes = ['', 'k', 'm', 'b', 't'];
-    let suffixNum = Math.floor(('' + value).length / 3);
-    let shortValue = '';
-    for (let precision = 2; precision >= 1; precision--) {
-      shortValue = parseFloat(
-        (suffixNum !== 0
-          ? value / Math.pow(1000, suffixNum)
-          : value
-        ).toPrecision(precision)
-      );
-      const dotLessShortValue = (shortValue + '').replace(
-        /[^a-zA-Z 0-9]+/g,
-        ''
-      );
-      if (dotLessShortValue.length <= 2) {
-        break;
-      }
-    }
-    if (shortValue % 1 !== 0) shortValue = shortValue.toFixed(1);
-    newValue = shortValue + suffixes[suffixNum];
-  }
-  return newValue;
-}
 
 const Image = ({ webformatURL, id, user, imageSize, add, fav, image }) => {
   const {
     favoritesState,
-    // favoritesDispatch,
     handleMouseEnter,
     handleMouseLeave,
     addToFavorites,
     removeFromFavorites,
     isImageInFavorites,
-    // switchFavorites,
   } = useContext(Context);
   const { hoveredImageId } = favoritesState;
-
-  //   const handleMouseEnter = () => {
-  //     setIsvisible(true);
-  //   };
-  //   const handleMouseLeave = () => {
-  //     setIsvisible(false);
-  //   };
 
   const handelClickedPlus = () => {
     if (isImageInFavorites(id)) return;
     else addToFavorites(image);
-    console.log(favoritesState);
   };
 
   const handelClickedHeart = () => {
@@ -63,8 +27,6 @@ const Image = ({ webformatURL, id, user, imageSize, add, fav, image }) => {
   return (
     <S.ContentImage
       img={webformatURL}
-      //   onMouseEnter={handleMouseEnter}
-      //   onMouseLeave={handleMouseLeave}
       onMouseEnter={() => handleMouseEnter(id)}
       onMouseLeave={handleMouseLeave}
     >
